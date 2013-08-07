fd7264f1b3eb4d36a1e848a54e59c06bf354970b
cjacks0413 <chjackson93@gmail.com>
2013-08-07 13:53:13 -0400

    create user profile page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,5 +10,11 @@ describe "UserPages" do
     it { should have_content('Sign Up') }
     it { should have_title(full_title('Sign Up')) }
   end
+end 
+
+describe "profile page" do
+  before { visit user_path(user) } 
   
+  it { should have_content(user.name) } 
+  it { should have_title(user.name) } 
 end 
