9890aae8a39fe4d0173001cea7a29fc15389ed04
cjacks0413 <chjackson93@gmail.com>
2013-08-08 10:54:32 -0400

    add signin capability

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Proj1::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   root 'static_pages#home' 
-  match '/signin', to: 'session#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
   match '/signout', to: 'sessions#destroy', via: 'delete' 
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
